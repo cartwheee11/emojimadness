fe76938fdde66d6a1e41cb72e1ff04354b3a048d
vane4ka <vane4ka@MBP-vane4ka.Dlink>
2024-01-30 21:21:04 +0300

    first

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped>
+@import './assets/Skeleton/css/normalize.css';
+@import './assets/Skeleton/css/skeleton.css';
+</style>
+
+<style>
+  .container {
+    margin: 0 auto;
+    max-width: 600px;
+  }
+</style>
---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import './assets/Skeleton/css/normalize.css';
+import './assets/Skeleton/css/skeleton.css';
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue' 
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
